dac7e8d06ae1ba2c0b85b09490d775ebeeefd544
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-06 12:58:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
+    "https://github.com/ProGamer097/GHOSTT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/naruto_support1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/naruto_support1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -141,21 +141,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
+    "PING_IMG_URL", "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+STATS_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+STREAM_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/eac463d4dccda3b184b6d.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
