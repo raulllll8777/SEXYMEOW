fda412b63a8e2e3515af9d266dc7c3618c6bd690
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-06 13:23:21 +0530

    Create Baka.py

---
+++ DAXXMUSIC/plugins/tools/Baka.py
@@ -0,0 +1,33 @@
+import requests
+import asyncio
+from pyrogram import filters
+from pyrogram.types import Message
+import time
+from DAXXMUSIC import app
+
+async def delete_message_after_timeout(message, timeout):
+    await asyncio.sleep(timeout)
+    await message.delete()
+
+@app.on_message(filters.command("baka"))
+async def es_img(_, message):
+    url = "http://api.nekos.fun:8080/api/baka"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+
+        data = response.json()
+        image = data.get("image")
+
+        if image:
+            sent_message = await message.reply_video(video=image, caption="BY @ushio_kofun_bot", parse_mode=None)
+            asyncio.create_task(delete_message_after_timeout(sent_message, 5))  # Delete after 1 minute
+        else:
+            await message.reply_text("No image found.")
+    except requests.RequestException as e:
+        # Handle request-related errors
+        await message.reply_text(f"Error: {str(e)}")
+    except Exception as e:
+        # Handle other unexpected errors
+        await message.reply_text(f"Unexpected error: {str(e)}")
