6be4dc7ce743bb863e18ae71be9f07e3884941fb
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-09 17:31:23 +0530

    Create Goodmorning.py

---
+++ DAXXMUSIC/plugins/Yumi/Goodmorning.py
@@ -0,0 +1,42 @@
+import re
+from dotenv import load_dotenv
+from pyrogram import filters
+import random
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from NARUTO import app
+
+
+
+# "/gm" command ka handler
+@app.on_message(filters.command("goodmorning", prefixes="g"))
+def goodnight_command_handler(client: Client, message: Message):
+    # Randomly decide whether to send a sticker or an emoji
+    send_sticker = random.choice([True, False])
+    
+    # Send a sticker or an emoji based on the random choice
+    if send_sticker:
+        client.send_sticker(message.chat.id, get_random_sticker())
+    else:
+        client.send_message(message.chat.id, get_random_emoji())
+
+# Function to get a random sticker
+def get_random_sticker():
+    stickers = [
+        "CAACAgUAAx0CcdFlwAABJEwtZhUtGNmL1wI9s-0LAt1y603baCcAAgcQAAJpOqlUOX594IPS_TYeBA",
+        "CAACAgUAAx0CcdFlwAABJEwtZhUtGNmL1wI9s-0LAt1y603baCcAAgcQAAJpOqlUOX594IPS_TYeBA",
+        "CAACAgUAAx0CcdFlwAABJEwtZhUtGNmL1wI9s-0LAt1y603baCcAAgcQAAJpOqlUOX594IPS_TYeBA",
+        "CAACAgUAAx0CcdFlwAABJEwtZhUtGNmL1wI9s-0LAt1y603baCcAAgcQAAJpOqlUOX594IPS_TYeBA",
+        "CAACAgUAAx0CcdFlwAABJEwtZhUtGNmL1wI9s-0LAt1y603baCcAAgcQAAJpOqlUOX594IPS_TYeBA",
+    ]
+    return random.choice(stickers)
+
+# Function to get a random emoji
+def get_random_emoji():
+    emojis = [
+        "🌤",
+        "🌤", 
+        "🌞",
+        
+    ]
+    return random.choice(emojis)
