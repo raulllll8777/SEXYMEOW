bd18b835dcdd6f594555ec2f7d976a3d13984682
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-09 22:35:13 +0530

    Update afk.py

--- DAXXMUSIC/plugins/tools/afk.py
+++ DAXXMUSIC/plugins/tools/afk.py
@@ -9,7 +9,7 @@ from DAXXMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 
-@app.on_message(filters.command(["afk", "brb","bye"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+@app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
 async def active_afk(_, message: Message):
     if message.sender_chat:
         return
