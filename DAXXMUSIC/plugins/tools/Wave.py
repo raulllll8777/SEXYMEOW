190244c57c35155abd1c4991e0802d21f476fa9d
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-10 18:31:22 +0530

    Create Wave.py

---
+++ DAXXMUSIC/plugins/tools/Wave.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+import requests
+import random
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup  
+from DAXXMUSIC import app
+
+regex_gif = ["wave"]
+pht = random.choice(regex_gif)
+url = f"https://api.waifu.pics/sfw/wave}"
+
+@app.on_message(filters.command("wave"))
+def get_waifu(client, message):
+    response = requests.get(url).json()
+    up = response['url']
+    if up:
+        message.reply_video(up,caption="BY @ushio_kofun_bot)
+    else:
+        message.reply("Request failed try /again")
