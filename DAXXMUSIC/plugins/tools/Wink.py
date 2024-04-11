9ea77334e7589d230ec2c889044d108a95516202
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-11 09:49:57 +0530

    Update Wink.py

--- DAXXMUSIC/plugins/tools/Wink.py
+++ DAXXMUSIC/plugins/tools/Wink.py
@@ -1,18 +1,1 @@
-from pyrogram import Client, filters
-import requests
-import random
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup  
-from DAXXMUSIC import app
 
-regex_gif = ["wink"]
-pht = random.choice(regex_gif)
-url = f"https://api.waifu.pics/sfw/wink}"
-
-@app.on_message(filters.command("wink"))
-def get_waifu(client, message):
-    response = requests.get(url).json()
-    up = response['url']
-    if up:
-        message.reply_video(up,caption="BY @ushio_kofun_bot)
-    else:
-        message.reply("Request failed try /again")
