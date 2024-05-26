4a5f6f1a1a1602ea671faf36a5ff4e393e11b73a
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-05-27 01:50:34 +0530

    Update sudoers.py

--- DAXXMUSIC/plugins/sudo/sudoers.py
+++ DAXXMUSIC/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("bhai Naruto sa maan lo sudo me dekha ni sakta owner ni mana Kiya hai meko soory", show_alert=True)
+        return await callback_query.answer("say inzuka is my master", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
