dfa9c63214d3af986355068c257d759d2cd67394
raulllll8777 <itsmesubham0007@gmail.com>
2024-05-29 16:01:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/naruto_support1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/naruto_support1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BILLA_GANG_NETWORK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BILLA_GANG_NTWK")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
